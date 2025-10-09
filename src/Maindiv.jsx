2d92f487fc467444b830dbe6b27121013de05c76
nooraalam1 <noorsomrat2000@gmail.com>
2025-10-09 13:37:37 +0600

    Navbar

--- src/Maindiv.jsx
+++ src/Maindiv.jsx
@@ -1,60 +1,37 @@
-import LightRays from './Aurora';
-import Shuffle from './Shuffle';
-import DecryptedText from './DecryptedText';
-import Rounded from './Rounded';
+import PillNav from './PillNav';
+
 
 export default function Maindiv() {
+    const menuItems = [
+      { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+      { label: 'About', ariaLabel: 'Learn about me', link: '/about' },
+      { label: 'Services', ariaLabel: 'View our services', link: '/services' },
+      { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
+    ];
+    
+
     return (
         <div>
-            <div className='relative'>
-                <LightRays
-                    raysOrigin="top-center"
-                    raysColor="#00ffff"
-                    raysSpeed={1.5}
-                    lightSpread={0.8}
-                    rayLength={1.2}
-                    followMouse={true}
-                    mouseInfluence={0.1}
-                    noiseAmount={0.1}
-                    distortion={0.05}
-                    className="custom-rays "
-                />
-            </div>
-            <div className=" absolute top-[40%]  left-[50%] -translate-x-[50%] -translate-y-[50%] lg:text-4xl font-bold text-white">
+           
 
-                <div>
-                    <DecryptedText
-                        text="Hello, This is"
-                        animateOn="view"
-                        revealDirection="center"
-                    />
-                </div>
+
+
+<div style={{ height: '100vh', background: '#1a1a1a' }}>
+  <PillNav
+    position="left"
+    items={menuItems}
+    menuButtonColor="#fff"
+    openMenuButtonColor="#fff"
+    changeMenuColorOnOpen={true}
+    colors={['#B19EEF', '#5227FF']}
+    accentColor="#ff6b6b"
+    onMenuOpen={() => console.log('Menu opened')}
+    onMenuClose={() => console.log('Menu closed')}
+  />
+
 
             </div>
-            <div className="absolute top-[50%]  left-[50%] -translate-x-[50%] -translate-y-[50%] lg:text-5xl font-bold text-white">
-                <div>
-                    <Shuffle
-                    text="Noor A Alam"
-                    shuffleDirection="right"
-                    duration={0.35}
-                    animationMode="evenodd"
-                    shuffleTimes={1}
-                    ease="power3.out"
-                    stagger={0.03}
-                    threshold={0.1}
-                    triggerOnce={true}
-                    triggerOnHover={true}
-                    respectReducedMotion={true}
-                    
-                />
-                </div>
-            </div>
-
-            <div className="absolute top-[40%]  left-[50%] -translate-x-[50%] -translate-y-[50%] ">
-                <div>
-                    <Rounded />
-                </div>
-            </div>
+            
         </div>
     )
 }
